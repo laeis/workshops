fd925dd7b4e8ea3bef26cb6dc8a93e6d60cee586
anovi <anovi@softserveinc.com>
2021-07-28 16:53:43 +0300

    add graceful shutdown

--- rest-api/cmd/scheduler-api/main.go
+++ rest-api/cmd/scheduler-api/main.go
@@ -11,6 +11,9 @@ import (
 	"github.com/sethvargo/go-envconfig"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 	"workshops/rest-api/internal/config"
 	handler "workshops/rest-api/internal/delivery/http"
 	"workshops/rest-api/internal/delivery/http/router"
@@ -26,12 +29,31 @@ func init() {
 }
 
 func main() {
-
 	ctx := context.Background()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		oscall := <-c
+		log.Printf("system call:%+v", oscall)
+		cancel()
+	}()
+
+	if err := serve(ctx); err != nil {
+		log.Printf("failed to serve:+%v\n", err)
+	}
+
+}
+
+func serve(ctx context.Context) (err error) {
+
 	//Init App config
 	var c config.AppConfig
 	if err := envconfig.Process(ctx, &c); err != nil {
-		log.Fatal(err)
+		return err
 	}
 	//init db
 	db := connectToDB(ctx, c)
@@ -56,7 +78,38 @@ func main() {
 	router.User(r.PathPrefix("/users").Subrouter(), userController, auhMiddleware)
 	router.Auth(r, authController, auhMiddleware)
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	srv := &http.Server{
+		Addr:    ":8000",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen:%+s\n", err)
+		}
+	}()
+
+	log.Printf("server started")
+
+	<-ctx.Done()
+
+	log.Printf("server stopped")
+
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	if err = srv.Shutdown(ctxShutDown); err != nil {
+		log.Fatalf("server Shutdown Failed:%+s", err)
+	}
+
+	log.Printf("server exited properly")
+
+	if err == http.ErrServerClosed {
+		err = nil
+	}
+	return
 }
 
 func connectToDB(ctx context.Context, c config.AppConfig) *sql.DB {
@@ -68,7 +121,7 @@ func connectToDB(ctx context.Context, c config.AppConfig) *sql.DB {
 	if err != nil {
 		panic(err)
 	}
-	err = db.Ping()
+	err = db.PingContext(ctx)
 	if err != nil {
 		panic(err)
 	}
